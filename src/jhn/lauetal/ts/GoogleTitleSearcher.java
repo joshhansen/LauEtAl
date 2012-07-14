3629f6356789a87219269bb793a13a3e677f76b0
Josh Hansen <hansen.joshuaa@gmail.com>
2012-07-14 15:57:04 -0600

    A title searcher using Google's deprecated AJAX API

---
+++ src/jhn/lauetal/ts/GoogleTitleSearcher.java
@@ -0,0 +1,114 @@
+package jhn.lauetal.ts;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import com.google.gson.Gson;
+
+public class GoogleTitleSearcher implements OrderedTitleSearcher {
+	private static final int NUM_RESULTS = 8;
+	private static final String API_BASE = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=" + NUM_RESULTS + "&q=";
+    private static final String CHARSET = "UTF-8";
+    
+    private final int maxTerms;
+	public GoogleTitleSearcher(int maxTerms) {
+		this.maxTerms = maxTerms;
+	}
+
+	@Override
+	public List<String> titles(String... terms) throws Exception {
+		StringBuilder search = new StringBuilder("site:en.wikipedia.org/wiki ");
+		search.append(terms[0]);
+		
+		if(terms.length > 1) {
+			for(int i = 1; i < Math.min(terms.length, maxTerms); i++) {
+				search.append(' ');
+				search.append(terms[i]);
+			}
+		}
+		
+	    URL url = new URL(API_BASE + URLEncoder.encode(search.toString(), CHARSET));
+	    Reader reader = new InputStreamReader(url.openStream(), CHARSET);
+	    GoogleResults results = new Gson().fromJson(reader, GoogleResults.class);
+	    
+	    List<String> titles = new ArrayList<String>();
+	    
+	    for(Result result : results.responseData.results) {
+	    	titles.add(cleanTitle(result.titleNoFormatting));
+	    }
+	    
+	    return titles;
+	}
+	
+	private static final Pattern wpTitleRgx = Pattern.compile("(.+?)( - Wiki.+)?( [.][.][.])?");
+	private static String cleanTitle(String title) {
+		return wpTitleRgx.matcher(title).replaceAll("$1");
+	}
+	
+	private static class GoogleResults {
+		private ResponseData responseData;
+
+		public ResponseData getResponseData() {
+			return responseData;
+		}
+
+		public void setResponseData(ResponseData responseData) {
+			this.responseData = responseData;
+		}
+
+		public String toString() {
+			return "ResponseData[" + responseData + "]";
+		}
+	}
+	
+	private static class ResponseData {
+		private List<Result> results;
+
+		public List<Result> getResults() {
+			return results;
+		}
+
+		public void setResults(List<Result> results) {
+			this.results = results;
+		}
+
+		public String toString() {
+			return "Results[" + results + "]";
+		}
+	}
+	
+	private static class Result {
+		private String titleNoFormatting;
+		private String url;
+		private String title;
+
+		public String getUrl() {
+			return url;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		
+		public void setTitleNoFormatting(String titleNoFormatting) {
+			this.titleNoFormatting = titleNoFormatting;
+		}
+
+		public String toString() {
+			return "Result[url:" + url + ",title:" + title + "]";
+		}
+	}
+}
